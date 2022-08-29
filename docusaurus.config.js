fd2b8164eeebee5a40fda88f01b79bed05f6c053
Brian Johnson <brianjo@fb.com>
2022-08-29 16:15:53 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'HG Commands',
   tagline: 'Dinosaurs are cool',
   url: 'https://brianjo.github.io/hgcommands/',
-  baseUrl: '/',
+  baseUrl: '/hgcommands/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
